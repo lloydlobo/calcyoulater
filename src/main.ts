3d0004832cef2206429b8b290c88e7c9aadb941c
lloydlobo <lloyd@lloydlobo.com>
2022-07-08 13:04:25 +0530

    chore: fix build process with missing app declaration

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,4 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 //   <h1>Hello Vite!</h1>
 //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 // `;
+console.log(app);
