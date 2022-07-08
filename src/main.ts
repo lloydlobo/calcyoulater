0161b681d6de37c3a40359da22886aa990fe6abb
lloydlobo <lloyd@lloydlobo.com>
2022-07-08 16:23:44 +0530

    feat: add regexp to add spaces & detect operators

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 // app.innerHTML = ` //   <h1>Hello Vite!</h1> //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> // `;
 
 console.log(app);
+const appClass = app.className;
+appClass.toLowerCase();
 // //////////////////////////////////////////////////////////////////////////
 
 // CONSTANTS
@@ -21,32 +23,63 @@ allButtons.forEach((btn) => {
 });
 
 console.dir(allBtn);
-
-allBtn.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    const value = btn.value;
-
-    getBtnValue(value);
-  });
-});
+let dataValidArray: number[] = [];
+let inputArray: string[] = [];
+let inputRegexArray: string[] = [];
 
 function getBtnValue(value: any) {
   let counterNum = 0; // for no. of clicks and keystrokes
   let counterNaN = 0;
 
+  let dataValid;
+
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+  /* 
+  // Before regular expressions can be used, they have to be compiled. This process allows them to perform matches more efficiently.
+  let re = /ab+c/i; // literal notation
+  let re = new RegExp('ab+c', 'i') // constructor with string pattern as first argument
+  let re = new RegExp(/ab+c/, 'i') // constructor with regular expression literal as first argument (Starting with ECMAScript 6)*/
+
+  const regExpNumAndOperator = /[+-]?(\d*\.\d+|\d+\.\d*|\d+)/gm; // gm -> global & multiline
   if (value) {
-    if (isNaN(value)) {
+    if (typeof value === "string") {
+      inputArray.push(value);
+
+      const inputJoin = inputArray.join("");
+      inputRegexArray.push(value);
+
+      let newStr = inputRegexArray
+        .join(" ")
+        .replace(regExpNumAndOperator, `$1`);
+      console.log(newStr);
+
+      outputDisplay.textContent = inputJoin;
+
+      const data = parseInt(value, 10);
+      dataValid = data;
+      dataValidArray.push(dataValid);
+
+      counterNum += 1;
+      return dataValidArray;
+    } else if (isNaN(value)) {
       outputDisplay.innerHTML = "NaN";
       counterNaN += 1;
       return NaN;
-    } else {
-      const data = parseInt(value, 10);
-      outputDisplay.textContent = data.toString();
-      counterNum += 1;
     }
   }
+  return dataValidArray;
   // throw new Error("Function not implemented.");
 }
+
+allBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const value = btn.value;
+
+    const validDataNumbers = getBtnValue(value);
+    // console.log(validDataNumbers);
+  });
+});
+
 //
 //
 //
@@ -54,18 +87,26 @@ function getBtnValue(value: any) {
 //
 //
 
-// GET TEXT
+// GET TEXT From User Input in DOM Display
 const outputVal = outputDisplay.textContent;
 
 export let output: number;
 if (outputVal) {
   output = parseInt(outputVal, 10);
-  console.log(output);
+  // console.log(output);
+}
+// Animate the canvas at intervals
+const animateCanvas = (
+  drawTime: number | undefined,
+  blankTime: number | undefined
+) => {
+  setInterval(drawLineOnCanvas, drawTime);
+  setInterval(fillBlankOnCanvas, blankTime);
+};
+
+// MAIN
+function main() {
+  animateCanvas(1000, 1000);
 }
 
-// //////////////////////////CANVAS//////////////////////////////////////////
-
-setInterval(drawLineOnCanvas, 1000);
-setInterval(fillBlankOnCanvas, 1000);
-
-// //////////////////////////////////////////////////////////////////////////
+main();
