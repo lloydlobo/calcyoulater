6f01646110aa39f75d72ebc8e7910d2ee6978623
lloydlobo <lloyd@lloydlobo.com>
2022-07-08 15:26:49 +0530

    feat: add canvas & button listener with values

---
+++ src/canvas/canvas.ts
@@ -0,0 +1,46 @@
+import { output } from "../main";
+
+// //////////////////////////CANVAS//////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////////////////////
+
+// https://html.spec.whatwg.org/multipage/canvas.html#best-practices
+
+const canvas = document.getElementById("visualizer") as HTMLCanvasElement;
+const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+canvas.style.boxShadow = `0 0 5px 10px hsla(223, 43%, 16%, 1)`; // blur the edges
+
+let lastX = context.canvas.width * Math.random();
+let lastY = context.canvas.height * Math.random();
+let hue = 0;
+export function drawLineOnCanvas() {
+  context.save();
+  context.translate(context.canvas.width / 2, context.canvas.height / 2);
+  context.scale(0.9, 0.9);
+  context.translate(-context.canvas.width / 2, -context.canvas.height / 2);
+
+  context.beginPath();
+  context.lineWidth = 1 + Math.sin(output * 10);
+  context.moveTo(lastX, lastY);
+  lastX = context.canvas.width * Math.random();
+  lastY = context.canvas.height * Math.random();
+  context.bezierCurveTo(
+    context.canvas.width * Math.random(),
+    context.canvas.height * Math.random(),
+    context.canvas.width * Math.random(),
+    context.canvas.height * Math.random(),
+    lastX,
+    lastY
+  );
+
+  hue = hue + 10 * Math.random();
+  context.strokeStyle = "hsl(" + hue + ", 50%, 50%)";
+  context.shadowColor = "white";
+  context.shadowBlur = 10;
+  context.stroke();
+  context.restore();
+}
+export function fillBlankOnCanvas() {
+  context.fillStyle = "hsla(223, 42%, 16%, 0.618)";
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,52 @@
+import { drawLineOnCanvas, fillBlankOnCanvas } from "./canvas/canvas";
 import "./style.css";
 
+// /////////////////////////APP//////////////////////////////////////////////
 const app = document.querySelector<HTMLDivElement>("#app")!;
+// app.innerHTML = ` //   <h1>Hello Vite!</h1> //   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a> // `;
 
-// app.innerHTML = `
-//   <h1>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `;
 console.log(app);
+// //////////////////////////////////////////////////////////////////////////
+
+// CONSTANTS
+const outputDisplay = document.querySelector("#output") as HTMLOutputElement;
+const btnAll = document.getElementsByTagName("button") as any;
+
+const allButtons = [...btnAll];
+
+let allBtn: HTMLButtonElement[] = [];
+
+allButtons.forEach((btn) => {
+  allBtn.push(btn);
+});
+
+console.dir(allBtn);
+
+allBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    console.log(btn.value);
+  });
+});
+
+//
+//
+//
+//
+//
+//
+
+// GET TEXT
+const outputVal = outputDisplay.textContent;
+
+export let output: number;
+if (outputVal) {
+  output = parseInt(outputVal, 10);
+  console.log(output);
+}
+
+// //////////////////////////CANVAS//////////////////////////////////////////
+
+setInterval(drawLineOnCanvas, 1000);
+setInterval(fillBlankOnCanvas, 1000);
+
+// //////////////////////////////////////////////////////////////////////////
