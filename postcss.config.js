18afc281852d8137327adaf1c306727f18fc1ef4
lloydlobo <lloyd@lloydlobo.com>
2022-07-09 16:54:50 +0530

    ci: add postcss-import & style layer, components

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    "tailwindcss/nesting": {},
     tailwindcss: {},
     autoprefixer: {},
   },
-}
+};
+
+// One important thing to note about postcss-import is that it strictly adheres to the CSS spec and disallows @import statements anywhere except at the very top of a file.
+// If you rather use postcss-nesting (which is based on the work-in-progress CSS Nesting specification), first install the plugin
+// npm install -D postcss-nesting
+// By default, it uses the postcss-nested plugin under the hood, which uses a Sass-like syntax and is the plugin that powers nesting support in the Tailwind CSS plugin API.
