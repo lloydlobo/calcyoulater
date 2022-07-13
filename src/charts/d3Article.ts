88e4682bb76f997dc213bbc73a5a602d58923d52
lloydlobo <lloyd@lloydlobo.com>
2022-07-13 19:41:40 +0530

    fix: unresolved imports & expected args

--- src/charts/d3Article.ts
+++ src/charts/d3Article.ts
@@ -1,13 +1,15 @@
-import { dataFetch, drawChart } from "./drawChart";
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { dataFetch, drawChart, sortBy } from "./drawChart";
 
 // ///////////////////////CHARTS D3//////////////////////////////////////////
 const d3Article = document.getElementById("d3");
 const d3Label = document.getElementById("d3Label") as HTMLLabelElement;
 if (!d3Article) throw new Error("d3 not found");
+// eslint-disable-next-line import/prefer-default-export
 export const d3Array: any[] = [];
 (async () => {
   await dataFetch; // use this to wait till the data is fetched // and then draw d3 chart when d3Array is populated in the stack/heap?  // FIXME
-  await drawChart(d3Array); // uses a global variable
+  await drawChart(d3Array, sortBy); // uses a global variable
   if (!d3Label) throw new Error("d3 not found");
   d3Label.textContent = "Market Cap";
 })();
