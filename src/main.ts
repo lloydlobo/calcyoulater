a8141f62ed4b0af5440e96ecc9eefdcda055ed1d
lloydlobo <lloyd@lloydlobo.com>
2022-07-18 12:50:25 +0530

    feat: filter number & operator with RegEx

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,8 @@ import { drawData } from "./charts/drawData";
 import { drawLineOnCanvas, fillBlankOnCanvas } from "./canvas/canvas";
 // eslint-disable-next-line import/no-unresolved, import/extensions
 import { isOperator } from "./utils/isOperator";
-// import { isANumberOnly } from "./utils/isANumberOnly";
-// import { isOperator } from "./utils/isOperator";
-// import { operatePrevCurr } from "./functions/operatePrevCurr";
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { operatePrevCurr } from "./functions/operatePrevCurr";
 
 // /////////////////////////APP///////
 const app = document.querySelector<HTMLDivElement>("#app")!;
@@ -17,46 +16,46 @@ appClass.toLowerCase();
 // ///////////////////////CHARTS D3///
 const d3Article = document.getElementById("d3");
 export const d3Label = document.getElementById("d3Label") as HTMLLabelElement;
-export const btnD3Category = document.getElementById("btnD3Category") as HTMLButtonElement; // prettier-ignore
+export const btnD3Category = document.getElementById(
+  "btnD3Category"
+) as HTMLButtonElement;
 
 if (!d3Article) throw new Error("d3 not found");
 export const d3Array: any[] = [];
-// D3 DATA RENDERING !!!!
-drawData();
+drawData(); // D3 DATA RENDERING !!!!
 
 // ////////////////CONSTANTS/////////
-export const outputDisplay = document.querySelector("#output") as HTMLOutputElement; // prettier-ignore
-export const inputHistory = document.querySelector("#inputHistory") as any;
-export const btnDecimal = document.getElementById("btnDecimal") as HTMLButtonElement; // prettier-ignore
-
 const btnAll: HTMLButtonElement[] = Array.from(
   document.getElementsByTagName("button")
 );
-
 const MAP_BTN_CACHE = new Map();
+for (let i = 0; i < btnAll.length; i += 1) MAP_BTN_CACHE.set(i, btnAll[i]);
 
-for (let i = 0; i < btnAll.length; i += 1) {
-  MAP_BTN_CACHE.set(i, btnAll[i]);
-}
-// eslint-disable-next-line no-console
-console.log(MAP_BTN_CACHE);
+export const outputDisplay = document.querySelector(
+  "#output"
+) as HTMLOutputElement;
+export const inputHistory = document.querySelector("#inputHistory") as any;
+export const btnDecimal = document.getElementById(
+  "btnDecimal"
+) as HTMLButtonElement;
+
 const btnClear = document.getElementById("btnClear") as HTMLButtonElement;
 const btnAllClear = document.getElementById("btnAllClear") as HTMLButtonElement;
 const btnClearArray = [btnClear, btnAllClear];
-const btnGetHistory = document.getElementById("btnHistory") as HTMLButtonElement; // prettier-ignore
-const btnCalculate = document.getElementById("btnResultEquals") as HTMLButtonElement; // prettier-ignore
-const btnBackspaceClear = document.getElementById("btnClear") as HTMLButtonElement; // prettier-ignore
-// const allBtn: HTMLButtonElement[] = [];
-// Spread btnAll to access forEach method
-// const allButtons = [...btnAll];
+const btnGetHistory = document.getElementById(
+  "btnHistory"
+) as HTMLButtonElement;
+const btnCalculate = document.getElementById(
+  "btnResultEquals"
+) as HTMLButtonElement;
+const btnBackspaceClear = document.getElementById(
+  "btnClear"
+) as HTMLButtonElement;
 
 // ////////////////CANVAS////////////
 const outputVal = outputDisplay.textContent;
 if (!outputVal) throw new Error("outputVal is not defined");
-
 export const output = parseInt(outputVal, 10); // animate canvas with a variable
-export const operatorIsClicked = (val: string) =>
-  val === "-" || val === "+" || val === "*" || val === "÷";
 
 // Animate the canvas at intervals
 const animateCanvas = (
@@ -71,13 +70,9 @@ const animateCanvas = (
 const drawCanvas = () => animateCanvas(1000, 1000);
 drawCanvas();
 
-// ////////////////DOM DISPLAY INPUT/////
-
-export const valString = "";
 // ////////////////LOGIC///////////////
-
-const MAIN_CACHE = {
-  clickCountNumAndOpBtn: 0,
+const DATA = {
+  countBtnClick: 0,
   MAP_VALUES_HANDLE: new Map(),
   MAP_FILTER_NUM: new Map(),
   MAP_FILTER_OP: new Map(),
@@ -88,11 +83,26 @@ const MAIN_CACHE = {
   countOperator: 0,
   markerOperator: 0,
   isBackspace: false,
-  MAP_DATA: new Map(),
+  MAP_DATA: new Map<number, string | number>(),
   MAP_NUMBERS: new Map<number, number>(),
   MAP_OPERATOR: new Map<number, string>(),
   MAP_BTN_UTIL_CACHE: new Map(), // AC, C, ..
 };
+
+const REGEX = {
+  numberDigitOnly: /([0-9])/gm,
+  operandsOnly: /(-|÷|\+|\*)/gm,
+  operandPositiveLookbehind: /.(?<=(-|÷|\*|\+))/, // gets all the operators groups
+};
+
+/* 
+
+/.(?<=(-|÷|\*|\+))/gm
+/(-|÷|\*|\+)/gm 
+*/
+
+export const operatorIsClicked = (val: string) =>
+  val === "-" || val === "+" || val === "*" || val === "÷";
 
 function isNumAndOperator(btn: HTMLButtonElement) {
   const curr = btn.value;
@@ -100,145 +110,199 @@ function isNumAndOperator(btn: HTMLButtonElement) {
   const matchCurrRegexNum = curr.match(regex)?.join("");
   const currIsOperator = isOperator(curr);
   let currIsNumber;
-  if (matchCurrRegexNum) {
+  if (matchCurrRegexNum)
     currIsNumber = typeof parseFloat(matchCurrRegexNum) === "number";
-  }
   if (currIsNumber) return true;
   if (currIsOperator) return true;
-  // if (!matchCurrRegexNum) return false; // disabled as regex isn't checking for operators
+
   return false;
 }
 
-function handleAllBtn(btn: HTMLButtonElement, clickCountBtn: number) {
+const prevValIsAlsoOperator = (value: string, count: number): boolean =>
+  count > 0 &&
+  isOperator(value) &&
+  isOperator(DATA.MAP_VALUES_HANDLE.get(count - 1));
+
+const prevValIsAlsoPeriod = (val: string, count: number): boolean =>
+  val === "." && count > 0 && DATA.MAP_VALUES_HANDLE.get(count - 1) === ".";
+
+function handleAllBtn(
+  btn: HTMLButtonElement,
+  clickCountBtn: number
+): Map<any, any> | undefined {
   const val = btn.value;
   const count = clickCountBtn;
+  if (prevValIsAlsoOperator(val, count)) return undefined;
+  if (prevValIsAlsoPeriod(val, count)) return undefined;
+  const currVal = DATA.MAP_VALUES_HANDLE.set(count, val);
 
-  const prevValIsAlsoPeriod = val === '.' && MAIN_CACHE.MAP_VALUES_HANDLE.get(count - 1) === '.' && count > 0;
-
-  if (prevValIsAlsoPeriod) return;
-  const currVal = MAIN_CACHE.MAP_VALUES_HANDLE.set(count, val);
   return currVal;
 }
 
-// keep track of when the operator is hit to set a marker
-// TODO get count of current number cache count and operator count and add both to marker count
 // When an operator is clicked
-function filterBtnInputs(fetchDataMap: Map<any, any>) {
+function filterBtnInputs(
+  fetchDataMap: Map<any, any>
+): (string | number)[] | undefined {
   let currOp;
   let currNum;
   let result;
-  const currData: string = fetchDataMap.get(MAIN_CACHE.clickCountNumAndOpBtn);
+  const currData: string = fetchDataMap.get(DATA.countBtnClick);
   if (typeof parseFloat(currData) === "number") {
-    const currCount = MAIN_CACHE.countFilterNumbers;
+    const currCount = DATA.countFilterNumbers;
     currNum = currData;
-    MAIN_CACHE.MAP_FILTER_NUM.set(currCount, currNum);
+    // DATA.MAP_FILTER_NUM.set(currCount, currNum);
     result = [currCount, currNum];
   }
   if (isOperator(currData)) {
-    const currCount = MAIN_CACHE.countFilterOperators;
+    const currCount = DATA.countFilterOperators;
     currOp = currData;
-    MAIN_CACHE.MAP_FILTER_OP.set(currCount, currOp);
+    // DATA.MAP_FILTER_OP.set(currCount, currOp);
     result = [currCount, currOp];
   }
 
   return result;
 }
 
-const getNumbers = (numbers: Map<number, number>) =>
-  numbers.get(MAIN_CACHE.countMainHub);
-const getOperators = (operator: Map<number, string>) =>
-  operator.get(MAIN_CACHE.countMainHub);
-
-function requestToOperate() {
-  if (!MAIN_CACHE.reqToCalculate) {
-    MAIN_CACHE.reqToCalculate = true;
-    return true;
+function handleCacheDataFilterMap(getValidNumAndOp: Map<any, any>): void {
+  const currData: string = getValidNumAndOp.get(DATA.countBtnClick);
+  if (typeof parseFloat(currData) === "number") {
+    const currCount = DATA.countFilterNumbers;
+    DATA.MAP_FILTER_NUM.set(currCount, currData);
   }
-  MAIN_CACHE.reqToCalculate = false;
-  return false;
+  if (isOperator(currData)) {
+    const currCount = DATA.countFilterOperators;
+    DATA.MAP_FILTER_OP.set(currCount, currCount);
+  }
+}
+
+// Populate Numbers & Operators in Cache
+function populateDataInCache(
+  getFilteredNumOpArr: (string | number)[] | undefined
+): void {
+  if (!getFilteredNumOpArr) throw new Error("string, num not found");
+  const currCount = DATA.countBtnClick;
+  DATA.MAP_DATA.set(currCount, getFilteredNumOpArr[1]);
 }
 
 // //// CENTRAL HUB FOR ALL PROCESSING ////
-function mainHubNumOp(btn: HTMLButtonElement) {
+function mainHubNumOp(btn: HTMLButtonElement): (string | number)[] | undefined {
   // #1 Handle & Filter Numbers
-  const getHandleValAndCount = handleAllBtn(btn, MAIN_CACHE.clickCountNumAndOpBtn); // prettier-ignore
-  if (!getHandleValAndCount) return
-  const getFilteredNumOpArr = filterBtnInputs(getHandleValAndCount);
-  console.log("file: main.ts | line 159 | mainHubNumOp | getFilteredNumOpArr", getFilteredNumOpArr);
+  const getValidNumAndOp = handleAllBtn(btn, DATA.countBtnClick);
+  if (!getValidNumAndOp) return undefined;
+  const getFilteredNumOpArr = filterBtnInputs(getValidNumAndOp);
+  if (!getFilteredNumOpArr) throw new Error("undefined");
 
-  if (!getFilteredNumOpArr) throw new Error("string, num not found or invalid or undefined");
+  // #! Set DATA
+  handleCacheDataFilterMap(getValidNumAndOp);
 
   // #2 Update count for num & op
-  if (isOperator(getFilteredNumOpArr[1])) MAIN_CACHE.countFilterOperators += 1; // prettier-ignore
-  // const resOperators = getOperators(MAP_OPERATOR);
-  else MAIN_CACHE.countFilterNumbers += 1;
-  // const resNumbers = getNumbers(MAP_NUMBERS);
+  if (isOperator(getFilteredNumOpArr[1])) DATA.countFilterOperators += 1;
+  else DATA.countFilterNumbers += 1;
+  DATA.countBtnClick += 1;
 
+  // #3 Populate Numbers & Operators in Cache
+  const currNumOp = getFilteredNumOpArr;
+  if (!currNumOp) throw new Error(`${currNumOp} is not found""`);
+  populateDataInCache(currNumOp);
 
-  MAIN_CACHE.clickCountNumAndOpBtn += 1;
-
-  return getFilteredNumOpArr; // this is enough. calculate in an async function separately
-}
-
-// #2 Populate Numbers & Operators in Cache
-function populateDataInCache(getFilteredNumOpArr: (string | number)[] | undefined) {
-  if (!getFilteredNumOpArr)
-    throw new Error("string, num not found");
-  const currCount = MAIN_CACHE.clickCountNumAndOpBtn;
-  MAIN_CACHE.MAP_DATA.set(currCount, getFilteredNumOpArr[1]);
+  return getFilteredNumOpArr; // this is enough. calculate rest in an async function
 }
 
 function updateDisplay(currNumOp: (string | number)[] | undefined) {
   if (!currNumOp) throw new Error("Cannot find mainHubNumOp result");
   outputDisplay.innerText =
-    outputDisplay.innerText.toString().trimEnd()
-    +
-    (currNumOp[1]).toString();
+    outputDisplay.innerText.toString().trimEnd() + currNumOp[1].toString();
+}
+
+function flattenDataMapCache(mappedData: Map<number, string | number>) {
+  const arrData: (string | number)[] = [];
+  const data: Map<number, string | number> = mappedData;
+  if (!data) throw new Error();
+  for (let i = 1; i <= data.size; i += 1) {
+    const currData = data.get(i);
+    if (!currData) throw new Error();
+    arrData.push(currData);
+  } // end of for loop
+  if (!arrData.filter((item) => (typeof item === "undefined") as boolean)) {
+    throw new Error("arrData has an undefined item");
+  }
+
+  return { data, arrData };
 }
 
 function compute() {
-  const len = MAIN_CACHE.MAP_DATA.size;
-  console.log("compute", { len }, (MAIN_CACHE.MAP_DATA))
-  let prev;
-  let curr;
-  let next;
-  let arrData = [];
-  const data = MAIN_CACHE.MAP_DATA
-  console.log({ data })
-  prev = data.get(1);
-  arrData.push(prev);
-  for (let i = 2; i <= len; i += 2) {
-    curr = data.get(i);
-    next = data.get(i + 1)
-    console.log({ prev, curr, next, data })
-    arrData.push(curr, next)
+  // #1 Flatten Map to Array
+  const mappedData = DATA.MAP_DATA;
+  const { data, arrData } = flattenDataMapCache(mappedData);
+
+  // #2 Convert Array to a string
+  const arrDataCopy = arrData; // (7) ['2', '2', '3', '3', '+', '6', '6']
+  const strJoinArrData = arrDataCopy.join("").trim(); // '2233+66'
+
+  // #3 Group up strings separated by operand
+  const regexIsNumber = REGEX.numberDigitOnly;
+  const regexIsOperands = REGEX.operandsOnly; // Learn RegEx in 20 minutes --> WebDevSimplified
+
+  const isNumber = regexIsNumber.test(strJoinArrData);
+  const isOperand = regexIsOperands.test(strJoinArrData);
+
+  const numbersFromString = strJoinArrData.replace(regexIsOperands, " ");
+  const operandsFromString = strJoinArrData.replace(regexIsNumber, " ");
+
+  const arrNumbers = numbersFromString.split(" ");
+  const arrOperands = operandsFromString
+    .split(" ")
+    .filter((item) => item !== " " && item !== "");
+
+  // eslint-disable-next-line no-console
+  console.log({ arrNumbers, arrOperands, numbersFromString, operandsFromString, }); // prettier-ignore
+
+  const strPrev = "1";
+  const strCurr = "2";
+  let strOperand = "+";
+
+  let floatPrev;
+  let floatCurr;
+  for (let i = 0; i < arrNumbers.length; i += 1) {
+    if (isNumber) {
+      if (!strPrev || !strCurr) throw new Error();
+      floatPrev = parseFloat(strPrev);
+      floatCurr = parseFloat(strCurr);
+    } else if (isOperand) {
+      if (!strOperand) throw new Error();
+      strOperand = strOperand.trim();
+    }
   }
-  console.log(arrData)
-  return [prev, curr, data, arrData]
+  if (!floatPrev || !floatCurr) throw new Error();
+
+  const result = operatePrevCurr(floatPrev, floatCurr, strOperand);
+
+  // eslint-disable-next-line no-console
+  console.log({ data, arrData, arrDataCopy, strJoinArrData, result });
+
+  return [data, arrData];
 }
 
 // ////////////// BTN EVENT LISTEN /////////////
+
 for (let i = 0; i < MAP_BTN_CACHE.size; i += 1) {
   const currBtn = MAP_BTN_CACHE.get(i);
-  if (isNumAndOperator(currBtn) || currBtn.value === '.') {
+  if (isNumAndOperator(currBtn) || currBtn.value === ".") {
     MAP_BTN_CACHE.get(i).addEventListener("click", () => {
       const clickedBtn: HTMLButtonElement = MAP_BTN_CACHE.get(i);
-
       // MAIN ENTRYPOINT
       const currNumOp = mainHubNumOp(clickedBtn);
-      if (!currNumOp) throw new Error(currNumOp + ' is not found""');
-      populateDataInCache(currNumOp);
-
       // DOM UI CLIENT STUFF
       if (!currNumOp) throw new Error("Cannot find mainHubNumOp() result");
       updateDisplay(currNumOp);
-    });
+    }); // end of event listener
   } else {
     const clickedUtilBtn: HTMLButtonElement = MAP_BTN_CACHE.get(i);
-
-    MAIN_CACHE.MAP_BTN_UTIL_CACHE.set(i, clickedUtilBtn);
+    DATA.MAP_BTN_UTIL_CACHE.set(i, clickedUtilBtn);
   }
-} // FIXME filter out clear buttons
+} // end of for loop
+
+// FIXME filter out clear buttons
 
 // Calculate result when "=" is entered/clicked
 btnCalculate.addEventListener("click", compute);
@@ -254,33 +318,20 @@ btnClearArray.forEach((btn) =>
 
 btnBackspaceClear.addEventListener("click", () => {
   // TODO
-  MAIN_CACHE.isBackspace = true;
+  DATA.isBackspace = true;
   // if isOperator
-  MAIN_CACHE.MAP_FILTER_OP.delete(MAIN_CACHE.countFilterOperators - 1);
-  MAIN_CACHE.countFilterOperators -= 1;
-  MAIN_CACHE.isBackspace = false;
+  DATA.MAP_FILTER_OP.delete(DATA.countFilterOperators - 1);
+  DATA.countFilterOperators -= 1;
+  DATA.isBackspace = false;
   // if isNumber
-  MAIN_CACHE.MAP_FILTER_NUM.delete(MAIN_CACHE.countFilterNumbers - 1);
-  MAIN_CACHE.countFilterNumbers -= 1;
-  MAIN_CACHE.isBackspace = false;
+  DATA.MAP_FILTER_NUM.delete(DATA.countFilterNumbers - 1);
+  DATA.countFilterNumbers -= 1;
+  DATA.isBackspace = false;
 });
 btnGetHistory.addEventListener("click", () => {
   // eslint-disable-next-line no-console
   console.log("gtHist");
 });
-
-// btnDecimal.removeEventListener(
-//   "click",
-//   () => {
-//     // eslint-disable-next-line no-console
-//     console.log(";remove");
-//   },
-//   false
-// );
-// btnDecimal.addEventListener("click", () => {
-//   // eslint-disable-next-line no-console
-//   console.log("addDecimal");
-// });
 
 // //// END OF SCRIPT ////
 
@@ -294,21 +345,56 @@ btnGetHistory.addEventListener("click", () => {
 ######## ##    ## ########
  */
 
-// Return value from eventlistener
-// const someObject = { aProperty: "Data" };
-// btnCalculate.addEventListener("click", () => {
-//   // eslint-disable-next-line no-console
-//   console.log(someObject.aProperty); // Expected Value: 'Data'
-//   someObject.aProperty = "Data Again"; // Change the value
-// });
-
-// window.setInterval(() => {
-//   if (someObject.aProperty === "Data Again") {
-//     // eslint-disable-next-line no-console
-//     console.log("Data Again: True");
-//     someObject.aProperty = "Data"; // Reset value to wait for next event execution
-//   }
-// }, 5000);
-
 // FIXME if the curr data or last data is number then delete what?
 // FIXME problem is we are'nt concatenating numbers yet. so gotta
+
+// TODO get count of current number cache count and operator count and add both to marker count
+
+/* 
+The Search of the fat cat street.
+12+22+424-44÷444*999  
+
+/(-|÷|\+|\*)/gm
+
+/(t|e|r){2,3}/gm
+/(t|e|r){2,3}\./gm  ==> eet.
+rerere re rere  => /(re){2,3}/gm
+
+.Hi ==> /^\./gm
+hi. ==> /\.$
+Lookbehind
+1. Positive lookbehinds /(?<=[tT]he)./gm
+
+Negative Lookbehind (?<![tT]he)
+
+Positive Lookahead (?=at)
+. matches any character (except for line terminators)
+
+Negative Lookahead (?!at)
+
+
+
+// PHONE NUMBERS
+
+9876543210
+1234567890
+123-456-7890
+123 456 7890
+(323) 456-7890
++1 123 456 7890  (9.)
+
+EXP MATCH
+1. /[0-9]{10}/gm  (in a row)
+2. /\d{10}/gm  (in a row)
+3. /\d{3}-?\d{3}-?\d{4}/gm  (optional -)
+4. /\d{3}\s?\d{3}\s?\d{4}/gm  (optional spaces)
+5. /\d{3}[\s-]?\d{3}[\s-]?\d{4}/gm
+6. /(\d{3})[\s-]?(\d{3})[\s-]?(\d{4})/gm  (capture digits)
+7. /(?<areacode>\d{3})[\s-]?(\d{3})[\s-]?(\d{4})/gm
+8. /\(?(?<areacode>\d{3})\)?[\s-]?(\d{3})[\s-]?(\d{4})/gm  (optional paranthesis with "\" escape char)
+9. /(?:(\+1)[ -])?\(?(?<areacode>\d{3})\)?[\s-]?(\d{3})[\s-]?(\d{4})/gm
+REPLACE
+1. $1$2$3  (groupings due to ( ... ) in regex)
+
+
+*/
